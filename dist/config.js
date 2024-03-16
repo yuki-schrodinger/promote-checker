68d16d553181931632362b42518480c06dcd1a4b
yuki-schrodinger <yuki@schrodingernft.ai>
2024-03-16 15:35:37 +0800

    feat: rm dist

--- dist/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.API_GET_EXPERIMENTS = exports.API_CREATE_EXPERIMENT = exports.BASE_URL = void 0;
-exports.BASE_URL = "http://35.188.108.156/experiments";
-exports.API_CREATE_EXPERIMENT = "/start-task";
-exports.API_GET_EXPERIMENTS = "/get-list";
--- dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dataTransformer_1 = require("./dataTransformer");
-dataTransformer_1.theProvider.prepareData();
