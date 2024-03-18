462d0d14a73a6e29a819ba3bf239bf4b562996f5
yuki-schrodinger <yuki@schrodingernft.ai>
2024-03-18 16:02:04 +0800

    feat: make params optional

--- src/server/model.ts
+++ src/server/model.ts
@@ -1,11 +1,11 @@
 export interface TextToImageRequest {
   mainSubject: string;
   traits: string;
-  negative_prompt: string;
-  steps: number;
-  batch_size: number;
-  width: number;
-  height: number;
-  n_iter: 1;
-  sampler_index: "DPM++ 2M Karras" | string;
+  negative_prompt?: string;
+  steps?: number;
+  batch_size?: number;
+  width?: number;
+  height?: number;
+  n_iter?: 1;
+  sampler_index?: "DPM++ 2M Karras" | string;
 }
